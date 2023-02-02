0e16865fe529dc25682913e475db8b3833ab9128
Kaseke <kasymkhankhubievnis@gmail.com>
2023-02-02 14:42:19 +0700

    changed the website's layout

--- bboard/views.py
+++ bboard/views.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 # from django.template import loader
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rubric
 
 def index(request: HttpRequest) -> HttpResponse:
     """
@@ -20,6 +20,16 @@ def index(request: HttpRequest) -> HttpResponse:
     """
     # template = loader.get_template('bboard/index.html')  # Возвращает экземпляр класса Template
     bbs = Bb.objects.all()  # .order_by('-published')  # Формируем контекст/содержимое шаблона - набор данных для вывода
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
     # return HttpResponse(template.render(context, request))  # рендерим содержимое веб страницы
     return render(request=request, template_name='bboard/index.html', context=context)
+
+
+def by_rubric(request: HttpRequest, rubric_id: int) -> HttpResponse:
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request=request, template_name='bboard/by_rubric.html', context=context)
